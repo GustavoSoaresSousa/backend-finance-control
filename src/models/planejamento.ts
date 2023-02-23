924e9a4165115c6ba5ce8c36098221a0afcd6f47
Gustavo Soares <gustavosoaresdev10@gmail.com>
2023-02-23 08:58:46 -0300

    Models de user e do planejamento de finanças criados

--- src/models/planejamento.ts
+++ src/models/planejamento.ts
@@ -2,41 +2,41 @@ import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export interface Receitas {
   salario: number;
-  outrasReceitas: number;
+  outrasReceitas: number | null;
 }
 export interface Despesas {
-  condominio: number;
-  aluguel: number;
+  condominio: number | null;
+  aluguel: number | null;
   transporte: number;
   alimentacao: number;
   luz: number;
   agua: number;
   internet: number;
-  academia: number;
-  saude: number;
-  cartaoDeCredito: number;
-  diarista: number;
-  escola : number;
-  emprestimo: number;
-  celular: number;
-  planoDeSaude: number;
+  academia: number | null;
+  saude: number | null;
+  cartaoDeCredito: number | null;
+  diarista: number | null;
+  escola : number | null;
+  emprestimo: number | null;
+  celular: number | null;
+  planoDeSaude: number | null;
   supermercado: number;
-  outros: number;
+  outros: number | null;
 }
 
 export interface Lazer {
-  alimentacao: number;
-  assinaturasMensais: number;
-  festasOuBares: number;
-  hobbies: number;
-  confraternizacao: number;
-  outros: number;
+  alimentacao: number | null;
+  assinaturasMensais: number | null;
+  festasOuBares: number | null;
+  hobbies: number | null;
+  confraternizacao: number | null;
+  outros: number | null;
 }
 
 export interface Investimentos {
-  educacao: number;
-  investimentos: number;
-  reservaDeEmergencia: number;
+  educacao: number | null;
+  investimentos: number | null;
+  reservaDeEmergencia: number | null;
 }
 
 export enum MES{
@@ -54,21 +54,24 @@ export enum MES{
   dezembro = 'Dezembro'
 }
 
-export interface DemostracoesFinanceiras extends Document {
+export interface DemostracoesFinanceiras {
+  _id?: string;
   receitas?: Receitas[]
   despesas?: Despesas[]
   lazer?: Lazer[]
   investimentos?: Investimentos[]
   mes: MES
+  ano: number
 }
 
-const schema = new Schema<DemostracoesFinanceiras>(
+const schema = new Schema(
   {
     receitas: { type: Array<Receitas>, require: true },
     despesas: { type: Array<Despesas>, require: true },
     lazer: { type: Array<Lazer>, require: false },
     investimentos: { type: Array<Investimentos>, require: false },
-    mes: { type: String, enum: Object.values(MES), require: true, unique: true }
+    mes: { type: String, enum: Object.values(MES), require: true, },
+    ano: {type: Number, required: true }
   },
   {
     toJSON: {
@@ -81,6 +84,7 @@ const schema = new Schema<DemostracoesFinanceiras>(
   }
 )
 
-export const PlanejamentoModel: Model<DemostracoesFinanceiras> = mongoose.model('Planejamento', schema)
+interface IPlanejamento extends Omit<DemostracoesFinanceiras, '_id'>, Document {}
+export const PlanejamentoModel: Model<IPlanejamento> = mongoose.model('Planejamento', schema)
 
 
